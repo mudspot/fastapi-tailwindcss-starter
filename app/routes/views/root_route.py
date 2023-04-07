ab5c5ee0b574b75e931fbea024730e0153089383
Terry Lee <terry.lee@playconcepts.co.uk>
2023-04-07 22:58:01 +0100

    Lint

--- app/routes/views/root_route.py
+++ app/routes/views/root_route.py
@@ -3,7 +3,6 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from app.core import global_state
-from app.core.global_config import app_config
 
 router = APIRouter()
 router.prefix = "/"
@@ -13,11 +12,17 @@ templates: Jinja2Templates = global_state.templates
 
 @router.get("", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request, "layout": "full_screen"})
+    return templates.TemplateResponse(
+        "login.html", {"request": request, "layout": "full_screen"}
+    )
+
 
 @router.get("home", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "layout": "vertical"})
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "layout": "vertical"}
+    )
+
 
 @router.get("messages", response_class=HTMLResponse)
 async def messages(request: Request):
